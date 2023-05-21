444ed24fdbb9fd22decd19042813b77dc7c35dda
ddingmin <ddingminyt@gmail.com>
2023-05-21 15:04:59 +0900

    feat: setting

---
+++ src/test/java/com/example/dogvote/DogVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogVoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
